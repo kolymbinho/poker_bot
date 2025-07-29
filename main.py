286868c4d165c8a8bc66d09a29d04463a0dd3bc3
kolymbinho <kolyambinho@gmail.com>
2025-07-29 16:50:29 +0300

    ццц

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-import os
-from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder
 from handlers import setup_handlers
+from dotenv import load_dotenv
+import os
 
 load_dotenv()
 TOKEN = os.getenv("TELEGRAM_TOKEN")
-PORT = int(os.getenv("PORT", "10000"))
+PORT = int(os.getenv("PORT", 10000))
 RENDER_URL = os.getenv("RENDER_URL")
 
-def main():
-    application = ApplicationBuilder().token(TOKEN).build()
-    setup_handlers(application)
-    application.run_webhook(
-        listen="0.0.0.0",
-        port=PORT,
-        webhook_url=f"{RENDER_URL}/{TOKEN}"
-    )
+app = ApplicationBuilder().token(TOKEN).build()
+setup_handlers(app)
 
 if __name__ == "__main__":
-    main()
+    print("Poker Bot запущен!")
+    app.run_webhook(
+        listen="0.0.0.0",
+        port=PORT,
+        url_path=TOKEN,
+        webhook_url=f"{RENDER_URL}/{TOKEN}"
+    )
