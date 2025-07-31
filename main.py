b586af6914108ede4d936a05d6c3081a374f5d16
kolymbinho <kolyambinho@gmail.com>
2025-07-31 13:52:29 +0300

    вроде рабочий

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 import os
-import sys
 from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder
 from handlers import setup_handlers
 import telegram
 
-# Загрузка переменных окружения
 load_dotenv()
-
-# Получаем токены и настройки из .env
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 PORT = int(os.getenv("PORT", 10000))
 RENDER_URL = os.getenv("RENDER_URL")
 
-# Отладочная информация
-print("Python version:", sys.version)
-print("PTB version:", telegram.__version__)
+print("Python version:", telegram.__version__)
 print("Poker Bot запущен!")
 
-# Создание и запуск приложения
 app = ApplicationBuilder().token(TOKEN).build()
 setup_handlers(app)
 
