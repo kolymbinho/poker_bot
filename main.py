58dc8447e9423be6fadaa7b443240ab7b8de99af
kolymbinho <kolyambinho@gmail.com>
2025-07-31 15:16:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from handlers import setup_handlers
 import os
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
-RENDER_URL = os.getenv("RENDER_URL")
 
 app = ApplicationBuilder().token(TOKEN).build()
+
 setup_handlers(app)
 
-app.run_webhook(
-    listen="0.0.0.0",
-    port=int(os.environ.get("PORT", 10000)),
-    webhook_url=f"{RENDER_URL}/{TOKEN}",
-)
+if __name__ == "__main__":
+    print("Poker Bot запущен!")
+    app.run_webhook(
+        listen="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000)),
+        webhook_url=f"{os.getenv('RENDER_URL')}/{TOKEN}"
+    )
